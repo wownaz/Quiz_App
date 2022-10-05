e22c46a6c4763ac92af300868f072ebbba0705e7
nazzzzzer <200103293@stu.sdu.edu.kz>
2022-10-05 13:15:42 +0600

    Display the size of the Affirmations list in a TextView

--- Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView2)
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
