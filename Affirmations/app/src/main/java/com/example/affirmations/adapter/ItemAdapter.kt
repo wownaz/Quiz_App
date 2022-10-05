9c4668693e8cb0d622a7d2350dbc675ca5c6aa0c
nazzzzzer <200103293@stu.sdu.edu.kz>
2022-10-05 14:35:16 +0600

    onCreateViewHolder

--- Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ Affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -12,20 +12,21 @@ import com.example.affirmations.model.Affirmation
 class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
 
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
 
         return ItemViewHolder(adapterLayout)
     }
-
-    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
-        val item = dataset[position]
-        holder.textView.text =  context.resources.getString(item.stringResourceId)
-    }
-
-    override fun getItemCount()= dataset.size
 }
+//    override fun onBindViewHolder(holder: ItemAdapter.ItemViewHolder, position: Int) {
+//        val item = dataset[position]
+//        holder.textView.text =  context.resources.getString(item.stringResourceId)
+//    }
+//
+//    override fun getItemCount()= dataset.size
+//}
