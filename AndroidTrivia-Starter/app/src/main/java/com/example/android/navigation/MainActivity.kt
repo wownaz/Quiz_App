01b74cddca9897d89f6f858a7d7add94edb346b0
nazzzzzer <200103293@stu.sdu.edu.kz>
2022-10-11 10:14:11 +0600

    display the navigation drawer

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
