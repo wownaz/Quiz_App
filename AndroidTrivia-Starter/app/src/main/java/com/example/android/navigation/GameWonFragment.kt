d9df35e4f9df90182eccf77f97a105022b19a8f6
nazzzzzer <200103293@stu.sdu.edu.kz>
2022-10-12 15:19:41 +0600

    Add an options menu to the Congratulations screen

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -40,6 +40,7 @@ class GameWonFragment : Fragment() {
         }
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
         Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        setHasOptionsMenu(true)
         return binding.root
     }
 }
